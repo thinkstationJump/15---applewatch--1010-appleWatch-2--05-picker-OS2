ddc00ca07135bf81556e6abc92068579531c5f73
TimZInGitHub <Tim@11234.local>
2015-12-02 16:13:30 +0800

    15资料-装逼特技(蓝牙+applewatch)-1010-appleWatch(2)-05-picker-OS2
    15资料-装逼特技(蓝牙+applewatch)-1010-appleWatch(2)-05-picker-OS2

--- 05-picker-OS2 WatchKit Extension/InterfaceController.m
+++ 05-picker-OS2 WatchKit Extension/InterfaceController.m
@@ -11,10 +11,61 @@
 
 @interface InterfaceController()
 
+/*
+ OS2新出的控件
+ - (void)focus;
+ - (void)resignFocus;
+ 
+ // Change the selected item index.
+ - (void)setSelectedItemIndex:(NSInteger)itemIndex;
+ 
+ - (void)setItems:(nullable NSArray<WKPickerItem *> *)items;
+ 
+.
+ - (void)setCoordinatedAnimations:(nullable NSArray<WKInterfaceObject<WKImageAnimatable> *> *)coordinatedAnimations;
+ .
+ - (void)setEnabled:(BOOL)enabled;
+ */
+@property (unsafe_unretained, nonatomic) IBOutlet WKInterfacePicker *pickerTest;
+
+/** items数组 */
+@property (nonatomic, strong) NSMutableArray *items;
+@property (unsafe_unretained, nonatomic) IBOutlet WKInterfacePicker *stackP;
+@property (unsafe_unretained, nonatomic) IBOutlet WKInterfacePicker *sp;
+
 @end
 
 
 @implementation InterfaceController
+
+- (NSMutableArray *)items
+{
+    if (!_items) {
+        _items = [NSMutableArray array];
+        
+        /*
+         @property (nonatomic, copy, nullable) NSString *title;.
+         @property (nonatomic, copy, nullable) NSString *caption;
+         =
+         @property (nonatomic, copy, nullable) WKImage *accessoryImage;
+         
+         @property (nonatomic, copy, nullable) WKImage *contentImage;
+
+         */
+        
+        for (NSInteger i = 0; i < 9; i++) {
+            
+            WKPickerItem *item = [[WKPickerItem alloc] init];
+            item.title = [NSString stringWithFormat:@"title%d", i];
+            item.caption = [NSString stringWithFormat:@"caption%d", i];;
+            item.accessoryImage = [WKImage imageWithImageData:UIImagePNGRepresentation([UIImage imageNamed:@"ad_00"])];
+            item.contentImage = [WKImage imageWithImage:[UIImage imageNamed:@"ad_01.png"]];
+            
+            [_items addObject:item];
+        }
+    }
+    return _items;
+}
 
 - (void)awakeWithContext:(id)context {
     [super awakeWithContext:context];
@@ -25,11 +76,31 @@
 - (void)willActivate {
     // This method is called when watch view controller is about to be visible to user
     [super willActivate];
+    
+    [self.pickerTest setItems:self.items];
+    [self.stackP setItems:self.items];
+    [self.sp setItems:self.items];
 }
 
 - (void)didDeactivate {
     // This method is called when watch view controller is no longer visible
     [super didDeactivate];
+}
+
+// 点击pikcer的某个item时候调用
+- (void)pickerDidFocus:(WKInterfacePicker *)picker
+{
+    NSLog(@"%s, line = %d", __FUNCTION__, __LINE__);
+}
+
+- (void)pickerDidResignFocus:(WKInterfacePicker *)picker
+{
+    NSLog(@"%s, line = %d", __FUNCTION__, __LINE__);
+}
+
+- (void)pickerDidSettle:(WKInterfacePicker *)picker
+{
+    NSLog(@"%s, line = %d", __FUNCTION__, __LINE__);
 }
 
 @end
